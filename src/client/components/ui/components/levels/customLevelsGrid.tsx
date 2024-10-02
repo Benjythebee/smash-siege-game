c01d3deeb09e395f2bf6addb97966be939b28bdc
Benjythebee <Benjy.larcher@aol.com>
2024-10-02 17:32:53 +1300

    add image

--- src/client/components/ui/components/levels/customLevelsGrid.tsx
+++ src/client/components/ui/components/levels/customLevelsGrid.tsx
@@ -1,10 +1,11 @@
 import { create } from 'zustand';
 import { CustomLevelsAPI } from '../../../../libs/customLevels/editorApi.js';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { LoadingIcon } from '../../icons/loader.js';
 import { LevelType } from '../../../../../common/types.js';
 import { convertLevelTypeToLevelData } from '../../../../../common/convert.js';
 import { useCustomLevelStore } from '../../../../libs/customLevels/customLevel.context.js';
+import { motion } from 'framer-motion';
 
 const levelsStore = create<{
   levels: LevelType[];
@@ -67,7 +68,6 @@ const useCustomLevels = () => {
 export const CustomLevels = (props: {}) => {
   const { levels, page, loading } = useCustomLevels();
   const { setPage } = useLevelFetchActions();
-  const { setLoadedCustomLevel } = useCustomLevelStore();
 
   const nextPage = () => {
     setPage(page + 1);
@@ -86,27 +86,9 @@ export const CustomLevels = (props: {}) => {
           </div>
         )}
         {levels.length === 0 && !loading && <div className="col-span-3 max-sm:col-span-2 text-white text-center text-2xl">No levels found</div>}
-        {levels.map((level) => {
-          const levelName = level.name;
-          const levelUploaded = new Date(level.created_at);
-          return (
-            <div
-              key={levelName}
-              onClick={() => {
-                setLoadedCustomLevel(level);
-              }}
-              className={`rounded-md py-2 px-4 max-sm:px-2 text-white flex flex-col gap-1 text-center font-bold  cursor-pointer round-md hover:bg-white/10`}
-            >
-              <div className="max-sm:text-sm text-lg uppercase text-ellipsis ">{level.name}</div>
-              <div className="max-sm:text-xxs text-xs flex gap-2">
-                <strong>Author: </strong> {level.author}
-              </div>
-              <div className="max-sm:text-xxs text-xs flex gap-2">
-                <strong>Uploaded:</strong> {levelUploaded.getMonth() + 1 + '-' + levelUploaded.getFullYear()}
-              </div>
-            </div>
-          );
-        })}
+        {levels.map((level) => (
+          <LevelCard level={level} key={level.author + level.id + level.name} />
+        ))}
       </div>
       <div className="flex justify-between w-full">
         {hasPrevPage ? (
@@ -120,6 +102,44 @@ export const CustomLevels = (props: {}) => {
           Next
         </button>
       </div>
+    </div>
+  );
+};
+
+const LevelCard = (props: { level: LevelType }) => {
+  const { setLoadedCustomLevel } = useCustomLevelStore();
+  const [hover, setHover] = useState(false);
+
+  const level = props.level;
+  const levelName = level.name;
+  const levelUploaded = new Date(level.created_at);
+  return (
+    <div
+      key={levelName}
+      onClick={() => {
+        setLoadedCustomLevel(level);
+      }}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+      onTouchStartCapture={() => setHover(true)}
+      onTouchEndCapture={() => setHover(false)}
+      className={`relative rounded-md max-sm:px-2 text-white flex flex-col gap-1 text-center font-bold overflow-hidden  cursor-pointer round-md hover:bg-white/10`}
+    >
+      <img src={level.image_url} className=" rounded-md bg-gray-600" alt={level.name} />
+      <motion.div
+        key={level.id + levelName}
+        initial={{ height: '1.5rem' }}
+        animate={hover ? { height: 'auto' } : 'initial'}
+        className="pointer-events-none absolute inset-x-0 bottom-0 bg-black/30"
+      >
+        <div className="max-sm:text-sm text-lg uppercase text-ellipsis ">{level.name}</div>
+        <div className="max-sm:text-xxs text-xs flex gap-2">
+          <strong>Author: </strong> {level.author}
+        </div>
+        <div className="max-sm:text-xxs text-xs flex gap-2">
+          <strong>Uploaded:</strong> {levelUploaded.getMonth() + 1 + '-' + levelUploaded.getFullYear()}
+        </div>
+      </motion.div>
     </div>
   );
 };
